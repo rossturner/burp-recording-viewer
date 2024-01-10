f1c376a25b32bebc66586fc34499c6dadb357b51
Ross Taylor-Turner <ross.taylor-turner@PSIT100489.local>
2024-01-10 15:12:52 +0000

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
+import {Icon} from "semantic-ui-react";
 
 function App() {
   return (
@@ -17,6 +18,7 @@ function App() {
         >
           Learn React
         </a>
+        <Icon loading name='certificate' />
       </header>
     </div>
   );
